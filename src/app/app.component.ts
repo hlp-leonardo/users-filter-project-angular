d4a3255748267683ccb6e31e01333dae3af6ac84
Hugo Pereira <hleonardo@outlook.com>
2024-06-06 20:45:07 +0100

    updating project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserInterface } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { FilterOptionInterface } from './interfaces/filter-options.interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -33,11 +34,42 @@ export class AppComponent implements OnInit {
     console.log(filterOptions);
     this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
   }
+
   filterUsersList(filterOptions: FilterOptionInterface, usersList: UserInterface[]): UserInterface[] {
     let filteredList: UserInterface[] = [];
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList)
+    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList)
     return filteredList;
   }
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, usersList: UserInterface[]): UserInterface[] {
+    const DATE_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if(DATE_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const checkDateInterval = (user: UserInterface) => isWithinInterval(new Date(user.registrationDate), {
+      start: startDate,
+      end: endDate
+    });
+
+    const list_filtered = usersList.filter(checkDateInterval);
+    return list_filtered;
+  }
+
+  filterUsersListByStatus(status: boolean | undefined, usersList: UserInterface[]): UserInterface[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+
+    if(STATUS_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.active === status);
+
+    return filteredList;
+  }
+
   filterUsersListByName(name: string | undefined, usersList: UserInterface[]): UserInterface[] {
     const NAME_NOT_TYPPED = name === undefined;
     
